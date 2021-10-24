75e7ab851b5209f5090edea6d2a82fec096df411
Gzim <gzimasanni@gmail.com>
2021-10-24 21:41:08 +0200

    Add basic setup

---
+++ src/components/expenseItem.js
@@ -0,0 +1,17 @@
+import ExpenseDate from './expenseDate';
+import './expenseItem.css'
+
+const expenseItem = (props) => {
+
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">{props.amount}</div>
+    </div>
+  )
+};
+
+export default expenseItem
