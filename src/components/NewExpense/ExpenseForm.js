d5c53c6e4712cef4056781b8c01d1ead650a04bc
Gzim <gzimasanni@gmail.com>
2021-10-25 13:26:20 +0200

    How group state!

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,12 +3,23 @@ import './ExpenseForm.css'
 
 const ExpenseForm = () => {
 
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('')
+  // const [enteredTitle, setEnteredTitle] = useState('');
+  // const [enteredAmount, setEnteredAmount] = useState('');
+  // const [enteredDate, setEnteredDate] = useState('')
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: '',
+  })
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value)
+    // setEnteredTitle(event.target.value)
+
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value
+    })
   }
 
   const amountChangeHandler = event => {
