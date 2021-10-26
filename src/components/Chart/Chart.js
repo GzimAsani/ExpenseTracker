db5f31c4edc9418af784cdd24348518ce6fcb958
Gzim <gzimasanni@gmail.com>
2021-10-26 12:13:32 +0200

    Add basic input for creating the chart

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,20 @@
+import './Chart.css';
+import ChartBar from './ChartBar';
+
+const Chart = () => {
+
+  return (
+    <div className='chart'>
+      {props.dataPoints.map((dataPoint) => (
+        < ChartBar 
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  )
+};
+
+export default Chart
---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,12 @@
+import './ChartBar.css';
+
+const ChartBar = () => {
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div className="chart-bar__fill"></div>
+      </div>
+      <div className="chart-bar__label"></div>
+    </div>
+  )
+}
