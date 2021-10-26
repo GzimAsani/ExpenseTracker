be3f98767d5af905174dfa57da3b9fe74bff008e
Gzim <gzimasanni@gmail.com>
2021-10-26 12:43:00 +0200

    created maximum value for each month

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,20 +1,23 @@
 import './Chart.css';
 import ChartBar from './ChartBar';
 
-const Chart = () => {
+const Chart = (props) => {
+
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value); 
+  const totalMaximum = Math.max(...dataPointValues)
 
   return (
-    <div className='chart'>
+    <div className="chart">
       {props.dataPoints.map((dataPoint) => (
-        < ChartBar 
+        <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
     </div>
-  )
+  );
 };
 
 export default Chart
