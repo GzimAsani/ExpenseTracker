67e5b2650da695a616d81c5e77dfb7282a155f60
Gzim <gzimasanni@gmail.com>
2021-10-26 12:38:45 +0200

    Update ExpensesChart to update values for each month

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -1,8 +1,29 @@
 import Chart from "../Chart/Chart";
 
 const ExpensesChart = (props) => {
+
+  const chartDataPoints = [
+    {label: 'Jan', value: 0},
+    {label: 'Feb', value: 0},
+    {label: 'Mar', value: 0},
+    {label: 'Apr', value: 0},
+    {label: 'May', value: 0},
+    {label: 'Jun', value: 0},
+    {label: 'Jul', value: 0},
+    {label: 'Aug', value: 0},
+    {label: 'Sep', value: 0},
+    {label: 'Oct', value: 0},
+    {label: 'Nov', value: 0},
+    {label: 'Dec', value: 0}
+  ];
+
+  for ( const expense in props.expenses ) {
+    const expenseMonth = expense.date.getMonth() // Starting at 0 for January
+    chartDataPoints[expenseMonth].value += expense.amount
+  }
+
   return (
-    < Chart />
+    < Chart dataPoints={chartDataPoints} />
   )
 };
 
